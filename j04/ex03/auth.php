7b7b5207f024b0c950d3f93f8832767b0c6d9a8f
Anne QUAN <aquan@e3r7p8.42.fr>
2019-03-14 10:18:00 +0100

    ex03 OK

---
+++ j04/ex03/auth.php
@@ -0,0 +1,22 @@
+<?php
+function auth($login, $passwd)
+{
+	$path = "../htdocs/private";
+	$file = "/passwd";
+	$pass = hash('whirlpool', $passwd);
+	if (!$login || !$passwd)
+		return FALSE;
+	$tab = unserialize(file_get_contents($path.$file));
+	if ($tab)
+	{
+		foreach ($tab as $data)
+		{
+			if ($data['login'] === $login && $data['passwd'] === $pass)
+				return true;
+		}
+	}
+	else
+		return false;
+}
+
+?>

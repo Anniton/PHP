dac330f24fba339195f4fa115d1048d2c52ca46d
Anne QUAN <aquan@e3r6p8.42.fr>
2019-03-21 17:14:00 +0100

    ex00 OK

--- j04/ex00/index.php
+++ j04/ex00/index.php
@@ -1,20 +1,16 @@
 <?php
-
-//Demarrer la session AVANT d'ecrire du code HTML
 session_start();
-if ($_GET['passwd'] && $_GET['login'] && $_GET['submit'] == OK)
+if ($_GET['passwd'] && $_GET['login'] && $_GET['submit'] && $_GET['submit'] === "OK")
 {
-	$_SESSION['prenom'] = $_GET['login'];
+	$_SESSION['login'] = $_GET['login'];
 	$_SESSION['passwd'] = $_GET['passwd'];
 }	
 ?>
-
 <html><body>
 <form method="GET" action="index.php">
-Identifiant: <input type="text" name="login" value="<?php echo $_SESSION['login']; ?>" />
+	Identifiant: <input type="text" name="login" value="<?php echo $_SESSION['login']; ?>" />
 	<br />
-	Mot de passe: <input type="password" name="passwd" value="<?php echo $_SESSION['login']; ?>" />
+	Mot de passe: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd']; ?>" />
  <input type="submit" name="submit" value="OK"/>
 </form>
 </body></html>
-

f01ca05be1667b4fff4ffab48fd1ac417a33db41
Anne QUAN <aquan@e2r6p17.42.fr>
2019-04-02 11:22:51 +0200

    ok

--- j05/ex01/ex01.sql
+++ j05/ex01/ex01.sql
@@ -1,6 +1,6 @@
 CREATE TABLE db_aquan.ft_table (
 	id INT(11) NOT NULL AUTO_INCREMENT ,
 	login VARCHAR(8) NOT NULL DEFAULT 'toto' ,
-	group ENUM('staff','student','other') NOT NULL ,
+	`group` ENUM('staff','student','other') NOT NULL ,
 	creation_date DATE NOT NULL ,
      PRIMARY KEY (id));
